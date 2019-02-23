312fa75d3f7c8d1f96c9a3838b86e2bfc7f73614
Enl1ghted <netrobber00@gmail.com>
2019-02-24 01:00:39 +0200

    VSTest

--- Core.cs
+++ Core.cs
@@ -3,6 +3,7 @@ using Kernel.Nexus;
 namespace Kernel {
     class Core {
         static void Main(string[] args) {
+            //- Nice -//
             Implementation.Initialization();
             Binder.RequestHandler.Request();
         }
